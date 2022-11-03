2c3c18fe0ed55f2a01735a41b89fff6d5f087a24
kingboof <105750814+kingboof@users.noreply.github.com>
2022-11-03 13:47:58 -0500

    map made

--- js/main.js
+++ js/main.js
@@ -4,18 +4,10 @@ function setup() {
 }
 
 function draw() {
-    for (let i = 0; i < 150; i++) {
-        loadImage('../assets/floortile.jpg', createMap);
-    }
+    
 }
 
 function windowResized() {
     // Resize the game when the size of the window is changed
     resizeCanvas(windowWidth, windowHeight);
 }
-
-function createMap(image_to_display) {
-    this.img = image_to_display;
-    this.img.resize(100,100)
-    alert(this.img)
-  }
